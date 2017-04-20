f88ed66dcda419f387a83d4ce79cd5b77c68010f
CXJ-HSM <851677152@qq.com>
2017-04-20 14:15:17 +0800

    Initial commit

---
+++ app/src/androidTest/java/com/example/cxj/actionbardemo/actionbardemoTest.java
@@ -0,0 +1,10 @@
+package com.example.cxj.actionbardemo;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by cxj on 2017/4/19.
+ */
+public class actionbardemoTest {
+
+}
---
+++ app/src/main/java/com/example/cxj/actionbardemo/actionbardemo.java
@@ -0,0 +1,93 @@
+package com.example.cxj.actionbardemo;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.icu.text.DateFormat;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewConfiguration;
+import android.widget.Toast;
+
+
+import java.lang.reflect.Field;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+
+public class actionbardemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        try {
+            //屏蔽真机的Menu来显示右边overflow button按钮
+            ViewConfiguration mconfig = ViewConfiguration.get(this);
+            Field menuKeyFidld;
+            Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+            if (menuKeyField != null) {
+                menuKeyField.setAccessible(true);
+                menuKeyField.setBoolean(mconfig, false);
+            }
+        } catch (NoSuchFieldException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO Auto-generated method stub
+        switch (item.getItemId()) {
+            // 弹出当前时间操作
+            case R.id.action_calendar:
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy年MM月dd日HH:mm:ss");
+                Date curDate = new Date(System.currentTimeMillis());// 获取当前时间
+                String str = formatter.format(curDate);
+                Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+                break;
+            // 拨打电话给10086
+            case R.id.action_call:
+                Intent intent = new Intent(Intent.ACTION_CALL,
+                        Uri.parse("tel:10086"));
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+
+                }
+                startActivity(intent);
+                break;
+            case R.id.action_msm:
+                //给10086发送内容为10086的信息
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage("10086", null, "10086", null, null);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+}
+
